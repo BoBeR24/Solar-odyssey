68fb2db5becd64ca744fe89deea12faf18a076e4
BoBeR24 <denver2004.dg@gmail.com>
2023-03-20 16:46:16 +0100

    added a semicolon

--- core/src/com/mygdx/game/SystemProperties.java
+++ core/src/com/mygdx/game/SystemProperties.java
@@ -5,7 +5,7 @@ import java.util.Hashtable;
 
 public class SystemProperties {
     final public static Hashtable<String, Integer> entities = new Hashtable<String, Integer>(){
-        {put("Sun", 0); put("Mercury", 1)}
+        {put("Sun", 0); put("Mercury", 1);}
     };
     final public static Vector[] coordinates = new Vector[]{new Vector(0.0, 0.0, 0.0)};
     final public static Vector[] velocities = new Vector[]{new Vector(0.0, 0.0, 0.0)};
