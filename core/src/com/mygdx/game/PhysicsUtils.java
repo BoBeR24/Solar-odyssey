9c27e799f488405bb744f8968d66630d1bb9e994
BoBeR24 <denver2004.dg@gmail.com>
2023-03-22 14:08:17 +0100

    changed actingForces to updateBody and make it so it immediately changes coordinate of the body.

--- core/src/com/mygdx/game/PhysicsUtils.java
+++ core/src/com/mygdx/game/PhysicsUtils.java
@@ -1,34 +1,30 @@
 package com.mygdx.game;
 
-// Calculates forces between a planet and Spacecraft
-
-import java.util.Hashtable;
-import java.util.Objects;
+// Calculates forces between a body and Spacecraft
 
 public class PhysicsUtils{
 
     //Still have to change
     public final static double gravitationalConstant = 6.6743 * Math.pow(10, -20);
     public final static int STEPSIZE = 3600;
-    private final static Hashtable<String, Integer> entities = SystemProperties.entities;
     private final static Vector[] velocities = SystemProperties.velocities;
     private final static Vector[] coordinates = SystemProperties.coordinates;
     private final static double[] masses = SystemProperties.masses;
 
-    public static Vector actingForces(celestialBody body){
+    public static void updateBody(celestialBody body){
         Vector forcesSum = new Vector(0.0, 0.0, 0.0); // sum of all forces
 
         for (celestialBody planet : SolarSystem.bodies){
-            if (planet == body || planet.getName().equals("Probe")) { // skips iteration where planet and body are the same object, because object can't affect itself
+            if (planet == body || body.getName().equals("Probe")) { // skips iteration where body and body are the same object, because object can't affect itself
                 continue;
             }
 
-                double scalingFactor = gravitationalConstant * planet.getMass() * body.getMass() * (-1);
+                double scalingFactor = gravitationalConstant * body.getMass() * body.getMass() * (-1);
 
-                Vector planetvector = planet.getLocation();
+                Vector bodyvector = body.getLocation();
                 Vector Spacecraftvector = body.getLocation();
 
-                Vector force = Spacecraftvector.subtract(planetvector);
+                Vector force = Spacecraftvector.subtract(bodyvector);
 
                 double magnitude = Math.pow(force.magnitude(), 3);
 
@@ -38,18 +34,19 @@ public class PhysicsUtils{
         }
 
 
-        return forcesSum;
+        updateVelocity(body, forcesSum);
+        updateCoordinate(body);
         
     }
     
-     public static void newVelocity(celestialBody planet, Vector finalForce){
-        int index = entities.get(planet.getName());
-        velocities[index].set(velocities[index].x + ((finalForce.x) * STEPSIZE) / masses[index], velocities[index].y +
-                ((finalForce.y) * STEPSIZE) / masses[index], velocities[index].z + ((finalForce.z) * STEPSIZE) / masses[index]);
+     private static void updateVelocity(celestialBody body, Vector forcesSum){
+        int index = body.getId();
+        velocities[index].set(velocities[index].x + ((forcesSum.x) * STEPSIZE) / masses[index], velocities[index].y +
+                ((forcesSum.y) * STEPSIZE) / masses[index], velocities[index].z + ((forcesSum.z) * STEPSIZE) / masses[index]);
     }
 
-    public void newCoordinate(celestialBody planet){
-        int index = entities.get(planet.getName());
+    private static void updateCoordinate(celestialBody body){
+        int index = body.getId();
         coordinates[index].set((coordinates[index].x + velocities[index].x) * STEPSIZE, (coordinates[index].y +
                 velocities[index].y) * STEPSIZE, (coordinates[index].z + velocities[index].z) * STEPSIZE);
 
