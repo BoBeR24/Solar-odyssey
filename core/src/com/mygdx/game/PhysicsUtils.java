24057373b13ebe099c47230918413fe85b96a80a
BoBeR24 <denver2004.dg@gmail.com>
2023-03-22 13:49:29 +0100

    merged Kees's physics formulas with current code(hope it works)

--- core/src/com/mygdx/game/PhysicsUtils.java
+++ core/src/com/mygdx/game/PhysicsUtils.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 // Calculates forces between a planet and Spacecraft
 
+import java.util.Hashtable;
 import java.util.Objects;
 
 public class PhysicsUtils{
@@ -9,6 +10,10 @@ public class PhysicsUtils{
     //Still have to change
     public final static double gravitationalConstant = 6.6743 * Math.pow(10, -20);
     public final static int STEPSIZE = 3600;
+    private final static Hashtable<String, Integer> entities = SystemProperties.entities;
+    private final static Vector[] velocities = SystemProperties.velocities;
+    private final static Vector[] coordinates = SystemProperties.coordinates;
+    private final static double[] masses = SystemProperties.masses;
 
     public static Vector actingForces(celestialBody body){
         Vector forcesSum = new Vector(0.0, 0.0, 0.0); // sum of all forces
@@ -37,13 +42,13 @@ public class PhysicsUtils{
         
     }
     
-     public void newVelocity(){
-        int index = entities.get(planet); 
+     public static void newVelocity(celestialBody planet, Vector finalForce){
+        int index = entities.get(planet.getName());
         velocities[index].set(velocities[index].x+ ((finalForce.x)*STEPSIZE)/masses[index], velocities[index].y+ ((finalForce.y)*STEPSIZE)/masses[index], velocities[index].z+ ((finalForce.z)*STEPSIZE)/masses[index]);
     }
 
-    public void newCoordinate(){
-        int index = entities.get(planet);
+    public void newCoordinate(celestialBody planet){
+        int index = entities.get(planet.getName());
         coordinates[index].set((coordinates[index].x+velocities[index].x)*STEPSIZE, (coordinates[index].y+velocities[index].y)*STEPSIZE, (coordinates[index].z+velocities[index].z)*STEPSIZE);
 
         
