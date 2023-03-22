1ddcdc87dda85f3309387e2ed57a1583186a05da
afiutowski <a.fiutowski@student.maastrichtuniversity.nl>
2023-03-22 13:26:04 +0100

    step size as a variable

--- core/src/com/mygdx/game/PhysicsUtils.java
+++ core/src/com/mygdx/game/PhysicsUtils.java
@@ -6,6 +6,7 @@ public class PhysicsUtils{
 
     //Still have to change
     public final static double gravitationalConstant = 6.6743 * Math.pow(10, -20);
+    public final static int STEPSIZE = 3600;
 
     public static Vector actingForces(celestialBody body){
         Vector forcesSum = new Vector(0.0, 0.0, 0.0); // sum of all forces
@@ -36,13 +37,12 @@ public class PhysicsUtils{
     
      public void newVelocity(){
         int index = entities.get(planet); 
-        velocities[index].set(velocities[index].x+ ((finalForce.x)*3600)/masses[index], velocities[index].y+ ((finalForce.y)*3600)/masses[index], velocities[index].z+ ((finalForce.z)*3600)/masses[index]);
-
+        velocities[index].set(velocities[index].x+ ((finalForce.x)*STEPSIZE)/masses[index], velocities[index].y+ ((finalForce.y)*STEPSIZE)/masses[index], velocities[index].z+ ((finalForce.z)*STEPSIZE)/masses[index]);
     }
 
     public void newCoordinate(){
         int index = entities.get(planet);
-        coordinates[index].set((coordinates[index].x+velocities[index].x)*3600, (coordinates[index].y+velocities[index].y)*3600, (coordinates[index].z+velocities[index].z)*3600);
+        coordinates[index].set((coordinates[index].x+velocities[index].x)*STEPSIZE, (coordinates[index].y+velocities[index].y)*STEPSIZE, (coordinates[index].z+velocities[index].z)*STEPSIZE);
 
         
     }
