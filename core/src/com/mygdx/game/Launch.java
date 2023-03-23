75ee16c8d69f02276a45c2298562c1f82da72ee7
BramDaemen <115256795+BramDaemen@users.noreply.github.com>
2023-03-23 11:09:00 +0100

    changed Launch and Probe files

--- core/src/com/mygdx/game/Launch.java
+++ core/src/com/mygdx/game/Launch.java
@@ -10,14 +10,15 @@ public class Launch {
     }
 
     final static double EARTHRADIUS = 6370;
+    final static double TITANRADIUS = 2575;
 
     
     // Launches an amount of probes
     public static Probe[] launchBunch(int amount, celestialBody earth, Vector longBoundryP, Vector shortBoundryP, int with, Vector v){
         
-        // making sure that the z factor is set to 0
-        longBoundryP.set(longBoundryP.x, longBoundryP.y, 0);
-        shortBoundryP.set(shortBoundryP.x, shortBoundryP.y, 0);
+        // making sure that the z factor is set to the same value
+        double zValue = longBoundryP.z;
+        shortBoundryP.set(shortBoundryP.x, shortBoundryP.y, zValue);
 
         // making sure that the launching positions are on earth
         if (longBoundryP.magnitude() - EARTHRADIUS != 0){longBoundryP.multiply(EARTHRADIUS/longBoundryP.magnitude());}
@@ -45,10 +46,55 @@ public class Launch {
             double z = Math.sin(alphaZ)*EARTHRADIUS;
             
             probes[i].setLocation(probes[i].getLocation().x + earth.getLocation().x, y + earth.getLocation().y, z + earth.getLocation().z);
-            
+            probes[i].setPSTART(probes[i].getLocation());
             probes[i].setVelocity(launch(earth, v).x, launch(earth, v).y, launch(earth, v).z);
-
+            probes[i].setVSRART(probes[i].getVelocity());
         }
         return probes;
+    }
+    // keeps track of launched probes and evaluates them.
+    public static Vector[] BunchLaunchPaths(Probe[] probes, celestialBody titan){
+        boolean[] isDrifting = new boolean[probes.length];
+        for (int i = 0; i < probes.length; i++){
+            double distanceOld = probes[i].getdistanceToTitan();
+            double distanceCurrent = Math.abs((probes[i].getLocation().add(titan.getLocation().multiply(-1))).magnitude()); // distances to titan
+            if (distanceCurrent <= TITANRADIUS){
+                System.out.println("TITAN HIT");
+                System.out.println("launch location :" + probes[i].getPStrart().toString());
+                System.out.println("launch velocity :" + probes[i].getVStart().toString());
+            }
+            else if (distanceCurrent > distanceOld){
+                probes[i].setDistanceToTitan(distanceCurrent);
+            } else {
+                isDrifting[i] = true;
+            }
+        }
+        boolean allAreDrifting = true;
+        for (int i = 0; i < isDrifting.length; i++){
+            if (!isDrifting[i]){
+                allAreDrifting = false;
+            }
+        }
+        if (allAreDrifting){
+            return evaluate(probes, titan);
+        } else return null;
+        
+    }
+
+    public static Vector[] evaluate(Probe[] probes, celestialBody titan){
+        Probe longBound = null;
+        Probe shortBound = null;
+        for (int i = 0; i < probes.length; i++){
+            if (probes[i].getLocation().magnitude() < titan.getLocation().magnitude()){
+                if (shortBound == null || probes[i].getdistanceToTitan() < shortBound.getdistanceToTitan()){
+                    shortBound = probes[i];
+                }
+            } else{
+                if (longBound == null || probes[i].getdistanceToTitan() < longBound.getdistanceToTitan()){
+                    longBound = probes[i];
+                }
+            }
+        }
+        return new Vector[] {longBound.getPStrart(), longBound.getVStart(), shortBound.getPStrart(), shortBound.getVStart()};
     }
 }
--- core/src/com/mygdx/game/Probe.java
+++ core/src/com/mygdx/game/Probe.java
@@ -5,7 +5,10 @@ public class Probe extends celestialBody{
     private double mass;
     private Vector location;
     private Vector velocity;
-    private String name;
+    private Vector pStart; // To track the starting position of the probe
+    private Vector vStart; // To track the startin velocity of the probe
+    private double distanceToTitan; // closest the probe has gotten to titan
+    
 
     Probe(String name) {
         super(name);
@@ -32,4 +35,27 @@ public class Probe extends celestialBody{
         return mass;
     }
 
+    public void setPSTART(Vector p){
+        this.pStart = p;
+    }
+
+    public Vector getPStrart(){
+        return pStart;
+    }
+
+    public void setVSRART(Vector v){
+        this.vStart = v;
+    }
+
+    public Vector getVStart(){
+        return vStart;
+    }
+
+    public double getdistanceToTitan(){
+        return distanceToTitan;
+    }
+
+    public void setDistanceToTitan(double distance){
+        this.distanceToTitan = distance;
+    }
 }
