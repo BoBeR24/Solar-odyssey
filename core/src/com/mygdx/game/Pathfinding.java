ed354a776928be24f330380ac58543bf772bf1a4
BramDaemen <115256795+BramDaemen@users.noreply.github.com>
2023-05-17 15:37:10 +0200

    groundwork for pathfinding

--- core/src/com/mygdx/game/Pathfinding.java
+++ core/src/com/mygdx/game/Pathfinding.java
@@ -3,4 +3,13 @@ package com.mygdx.game;
 //a class that can plot a path from any point to any celestial object, or a path to get in orbit of a celestial body.
 public class Pathfinding {
     
+    //Method that plots a path of a probe to a celestial body
+    public static void toBody(Probe probe, celestialBody body){
+
+    }
+
+    //Method that plots a path of a probe, so that it gets in orbit of the celestial body
+    public static void inOrbit(Probe probe, celestialBody body){
+
+    }
 }
