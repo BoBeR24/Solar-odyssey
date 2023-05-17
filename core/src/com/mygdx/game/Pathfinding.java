98c6c114dab79daf3bef3702ed7aa11a39b6da90
BramDaemen <115256795+BramDaemen@users.noreply.github.com>
2023-05-17 17:38:15 +0200

    finished tobody pathfinding

--- core/src/com/mygdx/game/Pathfinding.java
+++ core/src/com/mygdx/game/Pathfinding.java
@@ -7,19 +7,22 @@ public class Pathfinding {
     private static final int maxAccelerationPossible = 6*(10^3); //The maximum acceleration the spacecraft can produce if m = 5000kg.
     
     //Method that plots a path of a probe to within a given distance of a celestial body, considering a max velocity by wich the probe should arrive at the celectial body.
-    public static void toBody(Probe probe, celestialBody body, int maxVelocity, int distanceToObject){
+    public static void toBody(Probe probe, celestialBody body, int maxVelocity, int distanceToArrive){
         Vector directionToBody = body.getLocation().add((probe.getLocation().multiply(-1)));
-        double distanceToBody = Math.abs(directionToBody.magnitude());
+        double distanceToGoal = Math.abs(directionToBody.magnitude()) + distanceToArrive;
         double velocity = probe.getVelocity().magnitude();
         
         // figure out if speed adjustment is needed
         double maxAcceleration = 0;
         double maxVelocityPotential = probe.getVelocity().magnitude() + maxAccelerationPossible;
-        if(maxAccelerationPossible <= distanceToBody*maxVelocity){
-            maxAcceleration = maxVelocityPotential;
+        boolean needToSlowDown = false;
+        if(maxAccelerationPossible <= distanceToGoal*maxVelocity){
+            maxAcceleration = maxAccelerationPossible;
         } else {
-            maxAcceleration = maxAccelerationPossible/distanceToBody;
+            maxAcceleration = (maxAccelerationPossible/distanceToGoal) - velocity;
+            needToSlowDown = true;
         }
+        probe.setVelocity(probe.getVelocity().add(maxAcceleration));
     }
 
     //Method that plots a path of a probe, so that it gets in orbit of the celestial body
