ade7593b39c054ccd19c8e490db7217abc70f0d0
BramDaemen <115256795+BramDaemen@users.noreply.github.com>
2023-05-17 16:47:50 +0200

    first step in pathfinding

--- core/src/com/mygdx/game/Pathfinding.java
+++ core/src/com/mygdx/game/Pathfinding.java
@@ -2,10 +2,24 @@ package com.mygdx.game;
 
 //a class that can plot a path from any point to any celestial object, or a path to get in orbit of a celestial body.
 public class Pathfinding {
-    
-    //Method that plots a path of a probe to a celestial body
-    public static void toBody(Probe probe, celestialBody body){
 
+    private static final int maxForce = 3*(10^7);    //The maximum amount of force in Newton the spacecraft is capable of producing.
+    private static final int maxAccelerationPossible = 6*(10^3); //The maximum acceleration the spacecraft can produce if m = 5000kg.
+    
+    //Method that plots a path of a probe to within a given distance of a celestial body, considering a max velocity by wich the probe should arrive at the celectial body.
+    public static void toBody(Probe probe, celestialBody body, int maxVelocity, int distanceToObject){
+        Vector directionToBody = body.getLocation().add((probe.getLocation().multiply(-1)));
+        double distanceToBody = Math.abs(directionToBody.magnitude());
+        double velocity = probe.getVelocity().magnitude();
+        
+        // figure out if speed adjustment is needed
+        double maxAcceleration = 0;
+        double maxVelocityPotential = probe.getVelocity().magnitude() + maxAccelerationPossible;
+        if(maxAccelerationPossible <= distanceToBody*maxVelocity){
+            maxAcceleration = maxVelocityPotential;
+        } else {
+            maxAcceleration = maxAccelerationPossible/distanceToBody;
+        }
     }
 
     //Method that plots a path of a probe, so that it gets in orbit of the celestial body
--- core/src/com/mygdx/game/Rocketry.java
+++ core/src/com/mygdx/game/Rocketry.java
@@ -3,12 +3,11 @@ package com.mygdx.game;
 // class that gives a probe object rocket functionallity
 public class Rocketry {
 
-    private final int maxForce = 3*10^7;    //The maximum amount of force in Newton the spacecraft is capable of producing.
-    private final int maxAcceleration = 6*10^3; //The maximum acceleration the spacecraft can produce if m = 5000kg.
 
-    // Method to make a probe go brrr
+    // Method to make a probe go brrr, vector in km/s
     public static void thrust(Probe probe, Vector thrust){
         probe.setVelocity(probe.getVelocity().add(thrust));
 
     }
+
 }
