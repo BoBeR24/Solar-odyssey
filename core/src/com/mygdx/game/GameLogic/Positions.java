fa44acd06cd17ae2e4c1b0c9fe9fc4c7fc422ec3
BramDaemen <115256795+BramDaemen@users.noreply.github.com>
2023-06-16 11:24:28 +0200

    positions

---
+++ core/src/com/mygdx/game/GameLogic/Positions.java
@@ -0,0 +1,27 @@
+package com.mygdx.game.GameLogic;
+import com.mygdx.game.Objects.*;
+
+import java.util.HashMap;
+
+public class Positions {
+    private HashMap<String, Vector > positions;
+    private static Positions instance;
+    
+    private Positions(){
+        this.positions = new HashMap<>();
+    }
+
+    public static void addPosition(String key, Vector value){
+        if (Positions.instance == null){
+            instance = new Positions();
+        }
+        instance.positions.put(key, value);
+    }
+
+    public static synchronized Positions getInstance(){
+        if(instance == null){
+            instance = new Positions();
+        }
+        return instance;
+    } 
+}
