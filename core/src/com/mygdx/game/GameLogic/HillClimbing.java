6a5e76f5be8feb6e080da37130f57eea297385ea
BramDaemen <115256795+BramDaemen@users.noreply.github.com>
2023-06-16 15:59:33 +0200

    made plan in hillclimbing

--- core/src/com/mygdx/game/GameLogic/HillClimbing.java
+++ core/src/com/mygdx/game/GameLogic/HillClimbing.java
@@ -1,34 +1,40 @@
 package com.mygdx.game.GameLogic;
 
-import com.mygdx.game.GUI.SolarSystemScreen;
 import com.mygdx.game.Objects.Probe;
-import com.mygdx.game.Objects.celestialBody;
 import com.mygdx.game.Properties.SolarSystem;
 import com.mygdx.game.Properties.SystemProperties;
 import com.mygdx.game.SupportiveClasses.Resetter;
 import com.mygdx.game.SupportiveClasses.Timer;
 
 public class HillClimbing {
-    public static boolean hasCompletedIteration = false;
     public static int step = 1;
     public static Probe probe;
     public static Resetter resetter;
     public static Timer timer;
+    
+    private static final int SECONDS_IN_WEEK = 604800;
+    private static final int SECONDS_IN_YEAR = 31536000;
 
     public static void hillClimb() {
         // gives the probe a thrust
-        if (hasCompletedIteration && step == 1) {
-            //Pathfinding.toBody(probe, (celestialBody) SolarSystem.bodies.get(SystemProperties.TITAN), 400000);
-            
-            if (timer.getTimePassed() > 1000000) {
+        if (step == 1) { 
+            if (timer.getTimePassed() % SECONDS_IN_WEEK == 0);
+            Positions.addPosition("Titan" + timer.getTimePassed() / SECONDS_IN_WEEK, SolarSystem.bodies.get(SystemProperties.TITAN).getLocation());
+            if (timer.getTimePassed() >= SECONDS_IN_YEAR){
+                Resetter.resetSimulation();
                 step++;
             }
-            //System.out.println(step + " " + timer.getTimePassed());
 
         } else if (step == 2) {
-            resetter.resetSimulation();
-            System.out.println("reset");
-            step = 1;
+            //TODO launch probes at all different positions in every week recorded and find the most fuel efficient. Make sure to save thrust mapping
+
+        } else if (step == 3) {
+            //TODO launch probe with thrust mapping found in step 2
+
+        } else if (step == 4) {
+            //TODO get in orbit
+        } else if (step == 5) {
+            //TODO get to surface
         }
     }
 }
