184d7705be96d850afb6555329c19e87430686bd
BramDaemen <115256795+BramDaemen@users.noreply.github.com>
2023-06-16 15:39:11 +0200

    fixed it

--- core/src/com/mygdx/game/SupportiveClasses/Resetter.java
+++ core/src/com/mygdx/game/SupportiveClasses/Resetter.java
@@ -11,7 +11,9 @@ public class Resetter {
     public static void resetSimulation(){
         for (int i = 0; i < SolarSystem.bodies.size()-1; i++){
             SolarSystem.bodies.get(i).setLocation(SystemProperties.initCoordinates[i]);
+            SolarSystem.bodies.get(i).setNextLocation((SystemProperties.initCoordinates[i]));            
             SolarSystem.bodies.get(i).setVelocity(SystemProperties.initVelocities[i]);
+            SolarSystem.bodies.get(i).setNextVelocity(SystemProperties.initVelocities[i]);
         }
         ProbeLauncher.launch(new Vector(41.2384, -15.006862175, -3.183));
         HillClimbing.timer.reset();
