2b0fbcea7cddf8549a11fe9b627f6e02e97c4bf2
BoBeR24 <denver2004.dg@gmail.com>
2023-06-20 22:47:27 +0200

    camera movement is extracted to a separate class

---
+++ core/src/com/mygdx/game/PhysicsEngine/CameraUtils.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.PhysicsEngine;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
+import com.mygdx.game.GameLogic.FlightLogic;
+import com.mygdx.game.Objects.Vector;
+import com.mygdx.game.Properties.SolarSystem;
+import com.mygdx.game.Properties.SystemProperties;
+
+public class CameraUtils {
+    static int distFactor = SolarSystem.DIST_FACTOR;
+    /** method to keep camera centered at the first probe position(so camera follows the first probe)
+     * */
+    public static void moveCameraToProbe(OrthographicCamera camera, Vector3 screenCenter){
+
+        if (SolarSystem.probe != null) {
+            Vector toFollow = SolarSystem.bodies.get(SystemProperties.PROBE).getLocation(); // our custom vector
+            Vector3 toFollow_gdx = new Vector3(screenCenter.x + (float) (toFollow.x / distFactor), screenCenter.y + (float) (toFollow.y / distFactor), 0);
+
+            camera.position.set(toFollow_gdx);
+            camera.update();
+        }
+
+    }
+}
