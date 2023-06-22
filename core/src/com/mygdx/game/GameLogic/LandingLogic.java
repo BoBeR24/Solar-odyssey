0df7b61bc560f59325920da5c29384b669764275
BoBeR24 <57745804+BoBeR24@users.noreply.github.com>
2023-06-22 15:49:42 +0200

    Update LandingLogic.java

--- core/src/com/mygdx/game/GameLogic/LandingLogic.java
+++ core/src/com/mygdx/game/GameLogic/LandingLogic.java
@@ -88,14 +88,14 @@ public class LandingLogic {
                         ((LandingPad) SolarSystem.bodies.get(0)).getWidth() / 2.0f),
                 (float) (centerScreenCords.y + (SolarSystem.bodies.get(0).getLocation().y / distFactor) -
                         ((LandingPad) SolarSystem.bodies.get(0)).getHeight() / 2.0f),
-                ((LandingPad) SolarSystem.bodies.get(0)).getWidth() * sizeFactor, ((LandingPad) SolarSystem.bodies.get(0)).getHeight() * sizeFactor);
+                ((LandingPad) SolarSystem.bodies.get(0)).getWidth() / sizeFactor, ((LandingPad) SolarSystem.bodies.get(0)).getHeight() / sizeFactor);
 
         // draw Titan
         game.shape.setColor(Color.valueOf("#f2a900"));
         game.shape.ellipse((float) (centerScreenCords.x + (SolarSystem.bodies.get(1).getLocation().x / distFactor) -
                         ((Titan) SolarSystem.bodies.get(1)).getWidth() / 2.0f),
                 (float) (centerScreenCords.y + (SolarSystem.bodies.get(1).getLocation().y / distFactor) - ((Titan) SolarSystem.bodies.get(1)).getHeight() / 2.0f),
-                ((Titan) SolarSystem.bodies.get(1)).getWidth() * sizeFactor, ((Titan) SolarSystem.bodies.get(1)).getHeight() * sizeFactor);
+                ((Titan) SolarSystem.bodies.get(1)).getWidth() / sizeFactor, ((Titan) SolarSystem.bodies.get(1)).getHeight() / sizeFactor);
 
         // draw landing module
         game.shape.setColor(Color.PINK);
@@ -103,7 +103,7 @@ public class LandingLogic {
                         SolarSystem.landingModule.getWidth() / 2.0f),
                 (float) (centerScreenCords.y + (SolarSystem.landingModule.getLocation().y / distFactor) -
                         SolarSystem.landingModule.getHeight() / 2.0f),
-                SolarSystem.landingModule.getWidth() * sizeFactor, SolarSystem.landingModule.getHeight() * sizeFactor);
+                SolarSystem.landingModule.getWidth() / sizeFactor, SolarSystem.landingModule.getHeight() / sizeFactor);
 
     }
 
