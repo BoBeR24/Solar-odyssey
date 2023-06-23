79db2aa950fd25db1091c2556dfe2a516bf45a18
keeseijnden <kees.eijnden@gmail.com>
2023-06-23 14:12:29 +0200

    worked on controller

--- core/src/com/mygdx/game/PhysicsEngine/Controller.java
+++ core/src/com/mygdx/game/PhysicsEngine/Controller.java
@@ -1,8 +1,10 @@
 package com.mygdx.game.PhysicsEngine;
 
+import com.mygdx.game.Objects.LandingModule;
 import com.mygdx.game.Objects.Probe;
 import com.mygdx.game.Objects.Vector;
 import com.mygdx.game.Properties.SolarSystem;
+import com.mygdx.game.Properties.SystemProperties;
 
 public class Controller {
 
@@ -43,20 +45,47 @@ public class Controller {
         SolarSystem.landingModule.setNextVelocity(SolarSystem.landingModule.getVelocity().x + (thruster.x / SolarSystem.landingModule.getMass()) * PhysicsUtils.STEPSIZE, SolarSystem.landingModule.getVelocity().y + (thruster.y / SolarSystem.landingModule.getMass()) * PhysicsUtils.STEPSIZE, 0);
     }
 
-    private void updateCoordinates() {
-        SolarSystem.landingModule.setNextLocation(SolarSystem.landingModule.getLocation().x + SolarSystem.landingModule.getVelocity().x * PhysicsUtils.STEPSIZE, SolarSystem.landingModule.getLocation().y + SolarSystem.landingModule.getVelocity().y * PhysicsUtils.STEPSIZE, 0);
+    private static void updateCoordinates() {
+        SolarSystem.landingModule.setNextLocation(SolarSystem.landingModule.getLocation().x + SolarSystem.landingModule.getNextVelocity().x * PhysicsUtils.STEPSIZE, SolarSystem.landingModule.getLocation().y + SolarSystem.landingModule.getNextVelocity().y * PhysicsUtils.STEPSIZE, 0);
+    
     }
 
     private static void stabilize() {
-        thrusterTarget.set(forceWind.x, yCounterForce, new Vector(forceWind.x, yCounterForce, 0).getAngle(new Vector(0, 1, 0)));
-
-        changeAngle(thrusterTarget.z - SolarSystem.landingModule.getRotation());
+        double acceleration=0.0095;//have to decide this, it is the max acceleration we want to produce with our thrustter on 1 dimension
+        thrusterTarget.set(forceWind.x,yCounterForce,new Vector(forceWind.x,yCounterForce,0).getAngle(new Vector(0,1,0)));
+        changeAngle(thrusterTarget.z -SolarSystem.landingModule.getRotation());
         thruster.set(thrusterTarget);
+       //seperate and if f=ma thing is correct 2 lines lower combine  change angle lines, add first two above as else below the big if
+
+        if(SolarSystem.landingModule.getVelocity().x !=0 || SolarSystem.landingModule.getVelocity().y !=0){
+        thrusterTarget.set(thrusterTarget.add(new Vector(acceleration*SolarSystem.landingModule.getMass(),acceleration*SolarSystem.landingModule.getMass(),0)));
+        thrusterTarget.z= thrusterTarget.getAngle(new Vector(0,1,0));
+        changeAngle(thrusterTarget.z - SolarSystem.landingModule.getRotation());
+        double timex=SolarSystem.landingModule.getVelocity().x/acceleration;
+        double timey=SolarSystem.landingModule.getVelocity().y/acceleration;
+        while(timex!=0 && timey!=0){
+        timex=SolarSystem.landingModule.getVelocity().x/acceleration;
+        timey=SolarSystem.landingModule.getVelocity().y/acceleration;
+            if (timex>1){
+                SolarSystem.landingModule.setNextVelocity(SolarSystem.landingModule.getVelocity().x -acceleration,SolarSystem.landingModule.getVelocity().y,0);
+            }else{
+                SolarSystem.landingModule.setNextVelocity(0,SolarSystem.landingModule.getVelocity().y,0);
+            }
+            if (timey>1){
+              SolarSystem.landingModule.setNextVelocity(SolarSystem.landingModule.getVelocity().x ,SolarSystem.landingModule.getVelocity().y-acceleration,0);
+
+            }else{
+              SolarSystem.landingModule.setNextVelocity(SolarSystem.landingModule.getVelocity().x ,0,0);
+            }
+            updateCoordinates();
+        }
+        }
+
     }
 
     private static void changeAngle(double angleDifference) {
         angleDifference = Math.abs(angleDifference);
-        if (angleDifference <= 1) {
+        if (angleDifference <= 0.5) {
             SolarSystem.landingModule.setRotation(thrusterTarget.z);
         }
         if (angleDifference <= 2) {
