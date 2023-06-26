e40d752c7ce6163ed9f8633b7cb906ee108b4197
BramDaemen <115256795+BramDaemen@users.noreply.github.com>
2023-06-26 12:17:26 +0200

    hj

--- core/src/com/mygdx/game/GameLogic/Pathfinding.java
+++ core/src/com/mygdx/game/GameLogic/Pathfinding.java
@@ -3,6 +3,7 @@ package com.mygdx.game.GameLogic;
 import java.util.ArrayList;
 
 import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.mygdx.game.Objects.Body;
 import com.mygdx.game.Objects.Probe;
 import com.mygdx.game.Objects.Vector;
 import com.mygdx.game.Objects.celestialBody;
@@ -19,7 +20,7 @@ public class Pathfinding {
     private static final double PI = Math.PI;
 
     //Method that plots a path of a probe to within a given distance of a celestial body, considering a max velocity by wich the probe shousld arrive at the celectial body.
-    public static void toBody(Probe probe, celestialBody body, double pointToSlowDown) {
+    public static void toBody(Probe probe, Body body, double pointToSlowDown) {
 
         Vector velocity = probe.getNextVelocity();
         Vector directionToBody = body.getLocation().subtract(probe.getLocation());
@@ -40,7 +41,7 @@ public class Pathfinding {
     }
 
     //Method that plots a path of a probe, so that it gets in orbit of the celestial body
-    public static void inOrbit(Probe probe, celestialBody body) {
+    public static void inOrbit(Probe probe, Body body) {
         Vector velocity = probe.getVelocity();
         Vector directionToBody = body.getLocation().subtract((probe.getLocation()));
         double distanceToBody = directionToBody.magnitude();
@@ -61,23 +62,5 @@ public class Pathfinding {
             acceleration.multiply(MAXACCELERATIONPOSSIBLE / acceleration.magnitude());
         }
         Rocketry.thrust(probe, acceleration);
-    }
-        public static void planPathInTime(int week, Probe probe){
-        Vector positionTitan = Positions.getPosition(week);
-        Vector directionToTitan = positionTitan.subtract(probe.getLocation());
-        double distance = directionToTitan.magnitude();
-        int time = week * HillClimbing.SECONDS_IN_WEEK;
-        double averageSpeed = distance / time;
-        
-        // determine where to stop accelerating and where to start deaccelerating
-        Vector thrust = new Vector(directionToTitan.x,directionToTitan.y,directionToTitan.z);
-        if (HillClimbing.timer.getTimePassed() < averageSpeed/MAXACCELERATIONPOSSIBLE){
-            thrust.multiply(thrust.magnitude()/MAXACCELERATIONPOSSIBLE);
-        } else if (HillClimbing.timer.getTimePassed() < time - (averageSpeed/MAXACCELERATIONPOSSIBLE)){
-            thrust.multiply(0);
-        } else if (HillClimbing.timer.getTimePassed() < time){
-            thrust.multiply(thrust.magnitude()/MAXACCELERATIONPOSSIBLE*-1);
-        }
-        Rocketry.thrust(probe, thrust);
     }
 }
